d532eba3d8613669d4463502bdb8982c7bd0c502
mailorcanto <130517407+mailorcanto@users.noreply.github.com>
2023-11-13 13:25:24 -0300

    Update index.jsx

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -1,16 +1,31 @@
-import "./index.css";
+import Card from '@mui/material/Card';
+import CardActionArea from '@mui/material/CardActionArea';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
 
-const Card = ({ title, content, image, link }) => {
+const CardRepositorio = ({ title, content, image, link }) => {
   return (
-    <article className="card">
-      <img src={image} alt="imagem do card" />
-      <div>
-        <h2>{title}</h2>
-        <p>{content}</p>
-      </div>
-      <a href={link}>Acessar Repositório</a>
-    </article>
+    <Card sx={{ maxWidth: 300, margin: '17.5px', border: '2px solid black', borderColor: 'darkgrey', borderRadius: 4, textAlign: 'center', display: 'block' }}>
+      <CardActionArea href={link}>
+        <CardMedia
+          component="img"
+          height="100"
+          image={image}
+          alt={title}
+          
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h7">
+            {title}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {content} <br></br>clique para acessar
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
   );
 };
 
-export default Card;
+export default CardRepositorio;
