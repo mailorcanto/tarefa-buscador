09d7c36fe032a6321841f01f480afdc498db06b7
mailorcanto <130517407+mailorcanto@users.noreply.github.com>
2023-11-12 22:46:46 -0300

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Inicial from './pages/inicial/index'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Inicial />
+  </React.StrictMode>,
+)
---
+++ src/pages/inicial/index.jsx
@@ -0,0 +1,71 @@
+import { useState } from "react";
+import axios from "axios";
+import Card from "../../components/card";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
+function Dark() {
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <main><Inicial /></main>
+    </ThemeProvider>
+  );
+}
+
+function Inicial () {
+  const [nomeUsuario, setnomeUsuario] = useState(""); //input
+  const [listaRepositorios, setlistaRepositorios] = useState([]); //repositório(s) retornado(s)
+  
+  return (
+    <div className="inicial">
+      <h1>Busca de repositórios no Github</h1>
+      <input
+        type="text"
+        placeholder="Digite o nome do usuário"
+        value={nomeUsuario}
+        onChange={(e) => setnomeUsuario(e.target.value)} // atualiza o estado com o valor do input
+      />
+      <button onClick={() => carregaRepositorios(nomeUsuario, setnomeUsuario, setlistaRepositorios)}>
+        Buscar usuário
+      </button>
+      <div className="cards">
+        {listaRepositorios.map((repositorio) => (
+          // renderiza os repositórios do usuário em cards
+          <Card
+            key={repositorio.id}
+            title={repositorio.name}
+            content={repositorio.description}
+            image={repositorio.owner.avatar_url}
+            link={repositorio.html_url}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+// função para carregar os repositórios do usuário
+async function carregaRepositorios(nomeUsuario, setnomeUsuario, setlistaRepositorios) {
+  if (nomeUsuario) {
+    // se o input não estiver vazio
+    try {
+      const response = await axios.get(
+        `https://api.github.com/users/${nomeUsuario}/repos`
+      );
+      setlistaRepositorios(response.data); // atualiza o estado com os repositórios
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    setlistaRepositorios([]); // se o input estiver vazio, limpa tela
+  }
+}
+
+export default Dark;
